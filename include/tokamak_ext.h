be20e90423a1cbccd46a44406e4d51e034802a86
Ivgeni Segal <isegal@graphiq.com>
2016-11-12 20:20:58 -0800

    Added header for exposure of internal functionality.

---
+++ include/tokamak_ext.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+ *                                                                       *
+ * Tokamak Physics Engine, Copyright (C) 2002-2007 David Lam.            *
+ * All rights reserved.  Email: david@tokamakphysics.com                 *
+ *                       Web: www.tokamakphysics.com                     *
+ *                                                                       *
+ * This library is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the files    *
+ * LICENSE.TXT for more details.                                         *
+ *                                                                       *
+ *************************************************************************/
+
+
+#ifndef TOKAMAK_EXT_H_H
+#define TOKAMAK_EXT_H_H
+
+#include "../tokamaksrc/src/containers.h"
+#include "../tokamaksrc/src/scenery.h"
+
+#include "../tokamaksrc/src/collision.h"
+#include "../tokamaksrc/src/collision2.h"
+#include "../tokamaksrc/src/constraint.h"
+#include "../tokamaksrc/src/rigidbody.h"
+
+#endif //TOKAMAK_EXT_H_H
