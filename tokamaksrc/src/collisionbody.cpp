bd02e2dd357be18a472af790a10a13b0c4358cf1
Ivgeni Segal <isegal@graphiq.com>
2016-11-25 19:53:37 -0800

    Refactor: Moved neCollisionBody methods to collisionbody.cpp.

--- tokamaksrc/src/collisionbody.cpp
+++ tokamaksrc/src/collisionbody.cpp
@@ -25,7 +25,8 @@
 #include "collision.h"
 #include "constraint.h"
 #include "rigidbody.h"
-
+#include "scenery.h"
+#include "simulator.h"
 
 /****************************************************************************
 *
@@ -67,4 +68,111 @@ void neCollisionBody::Free() {
     neRigidBodyBase::Free();
 
     RemoveConstraintHeader();
+}
+
+/****************************************************************************
+*
+*	neCollisionBody::RemoveGeometry
+*
+****************************************************************************/
+
+bool neCollisionBody::RemoveGeometry(neGeometry *g) {
+    if (!col.convex)
+        return false;
+
+    TConvexItem *gi = (TConvexItem *) col.convex;
+
+    while (gi) {
+        TConvex *convex = reinterpret_cast<TConvex *>(gi);
+
+        gi = gi->next;
+
+        if (convex == reinterpret_cast<TConvex *>(g)) {
+            if (col.convex == convex) {
+                col.convex = (TConvex *) gi;
+            }
+
+            sim->geometryHeap.Dealloc(convex, 1);
+
+            col.convexCount--;
+
+            if (col.convexCount == 0) {
+                col.convex = nullptr;
+
+                if (IsInRegion() && !isCustomCD)
+                    sim->region.RemoveBody(this);
+            }
+
+            return true;
+        }
+    }
+    return false;
+}
+
+/****************************************************************************
+*
+*	neCollisionBody::BreakGeometry
+*
+****************************************************************************/
+
+neRigidBody *neCollisionBody::BreakGeometry(neGeometry *g) {
+    neRigidBody *newBody = sim->CreateRigidBodyFromConvex((TConvex *) g, this);
+    return (neRigidBody *) newBody;
+}
+
+/****************************************************************************
+*
+*	neCollisionBody::UseCustomCollisionDetection
+*
+****************************************************************************/
+
+void neCollisionBody::UseCustomCollisionDetection(bool yes, const neT3 *obb, neReal boundingRadius) {
+    if (yes) {
+        this->obb = *obb;
+
+        col.boundingRadius = boundingRadius;
+
+        isCustomCD = yes;
+
+        if (isActive && !IsInRegion()) {
+            sim->region.AddBody(this, nullptr);
+        }
+    } else {
+        isCustomCD = yes;
+
+        this->UpdateBoundingInfo();
+
+        if (IsInRegion() && GetGeometryCount() == 0) {
+            sim->region.RemoveBody(this);
+        }
+    }
+}
+
+
+/****************************************************************************
+*
+*	neCollisionBody::RemoveSensor
+*
+****************************************************************************/
+
+bool neCollisionBody::RemoveSensor(neSensor *s) {
+    if (!sensors)
+        return false;
+
+    neSensorItem *si = (neSensorItem *) sensors;
+
+    while (si) {
+        neSensor_ *sensor = (neSensor_ *) si;
+
+        si = si->next;
+
+        if (sensor == reinterpret_cast<neSensor_ *>(s)) {
+            //reinterpret_cast<neSensorItem *>(s)->Remove();
+
+            sim->sensorHeap.Dealloc(sensor, 1);
+
+            return true;
+        }
+    }
+    return false;
 }
